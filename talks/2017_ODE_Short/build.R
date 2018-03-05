cfeee7036f0e951cf37b5575add87b75a13c95d7
raphaelvolz <rv@nogago.com>
2018-03-05 20:47:33 +0100

    2nd attempt to upload
    old Azure site

---
+++ talks/2017_ODE_Short/build.R
@@ -0,0 +1,4 @@
+library(rmarkdown)
+render("Presentation.Rmd", "revealjs::revealjs_presentation")
+#render("Presentation.Rmd", "pdf_document" )
+
---
+++ talks/2017_Open Data Ecosystem/build.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+render("Presentation.Rmd", "revealjs::revealjs_presentation")
+
